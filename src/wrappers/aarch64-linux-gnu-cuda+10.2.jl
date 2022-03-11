8e7c71df7329031d4487b41f68368fb3d51e1184
jlbuild <juliabuildbot@gmail.com>
2022-03-11 12:33:25 +0000

    CUDNN_jll build 8.2.4+0

--- src/wrappers/aarch64-linux-gnu-cuda+10.2.jl
+++ src/wrappers/aarch64-linux-gnu-cuda+10.2.jl
@@ -8,7 +8,7 @@ function __init__()
     JLLWrappers.@generate_init_header(CUDA_loader_jll)
     JLLWrappers.@init_library_product(
         libcudnn,
-        "lib/libcudnn.so.8",
+        "lib/libcudnn.so",
         nothing,
     )
 
